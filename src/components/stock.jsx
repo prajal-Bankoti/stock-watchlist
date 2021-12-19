8394f318590dc288276b887bf6977e76d898016a
prjal-Bankoti <prajalbankoti971@gmail.com>
2021-12-19 19:30:28 +0530

    initial commit

---
+++ src/components/stock.jsx
@@ -0,0 +1,259 @@
+import { useState, useEffect } from "react";
+import "./stock.css";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import AddIcon from "@mui/icons-material/Add";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const axios = require("axios");
+
+export default function Stoks() {
+  const [stockdata, setStockata] = useState([]);
+  const [update, setUpdate] = useState(false);
+  const [search, setSearch] = useState([]);
+  const [show, setShow] = useState([]);
+  const [hover, setHover] = useState([]);
+  const [watch, setWach] = useState(false);
+  useEffect(() => {
+    async function showData() {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:3005/stocksData?list=true"
+        );
+        setStockata(data);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    showData();
+  }, [update]);
+  async function showSearchData() {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:3005/stocksData?q=${search}&_page=1&_limit=30`
+      );
+      setShow(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async function showUpdate(e) {
+    try {
+      await axios.patch(`http://localhost:3005/stocksData/${e.id}`, {
+        list: e.list ? false : true,
+      });
+      await showSearchData();
+      setUpdate(update ? false : true);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div
+      className="box"
+      onScroll={(p) => {
+        console.log("p", p);
+      }}
+      onClick={(el) => {
+        console.log(el.target.nodeName);
+        if (el.target.nodeName !== "svg" && el.target.nodeName !== "INPUT") {
+          setWach(false);
+          setSearch("");
+        }
+      }}
+    >
+      <div id="main-box">
+        <div id="input">
+          <div> COUNT {stockdata.length}</div>
+          <h3>{watch ? "ADD WATHLIST..." : "YOUR WATHLIST "}</h3>
+          <input
+            onChange={(e) => {
+              setSearch(e.target.value);
+              setWach(true);
+              showSearchData();
+            }}
+            value={search}
+            onKeyPress={(en) => {
+              if (en.code === "Enter") {
+                setWach(true);
+                showSearchData();
+                console.log(en.code);
+              }
+              console.log(en.code, "o");
+            }}
+            type="text"
+            placeholder="Search Stocks..."
+            id=""
+          />
+        </div>
+        <div style={{ width: "100%", height: "85px" }}></div>
+        <div>
+          {watch
+            ? show.map((el) => (
+                <div
+                  key={`${el.id}A`}
+                  onMouseOver={() => {
+                    setHover(el.id);
+                  }}
+                  onMouseOut={() => {
+                    setHover();
+                  }}
+                  className="stock-list"
+                >
+                  <div
+                    style={{
+                      color: el.todayPrice < el.yesPrice ? "#df514c" : "green",
+                    }}
+                  >
+                    {el.companyName}
+                  </div>
+                  <div>{el.stockExchange}</div>
+                  <div>
+                    {el.yesPrice - el.todayPrice < 0
+                      ? (
+                          ((el.todayPrice - el.yesPrice) / el.yesPrice) *
+                          100
+                        ).toFixed(2)
+                      : (
+                          ((el.yesPrice - el.todayPrice) / el.yesPrice) *
+                          100
+                        ).toFixed(2)}{" "}
+                    %
+                  </div>
+                  {el.todayPrice < el.yesPrice ? (
+                    <KeyboardArrowDownIcon
+                      style={{
+                        color:
+                          el.todayPrice < el.yesPrice ? "#df514c" : "green",
+                      }}
+                    />
+                  ) : (
+                    <KeyboardArrowUpIcon
+                      style={{
+                        color:
+                          el.todayPrice < el.yesPrice ? "#df514c" : "green",
+                      }}
+                    />
+                  )}
+
+                  <div
+                    style={{
+                      color: el.todayPrice < el.yesPrice ? "#df514c" : "green",
+                    }}
+                  >
+                    {el.todayPrice}
+                  </div>
+                  <div
+                    className="stock-pop"
+                    style={{
+                      display: hover === el.id ? "block" : "none",
+                    }}
+                  >
+                    <ErrorOutlineIcon />
+                    {el.list ? (
+                      <DeleteOutlineIcon
+                        onClick={(e) => {
+                          showUpdate(el);
+                          console.log(e);
+                        }}
+                      />
+                    ) : (
+                      <AddIcon
+                        onClick={() => {
+                          showUpdate(el);
+                          console.log(el);
+                        }}
+                      />
+                    )}
+                    <div>N</div>
+                    <div>B</div>
+                  </div>
+                </div>
+              ))
+            : stockdata.map((el) => (
+                <div
+                  key={`${el.id}A`}
+                  onMouseOver={() => {
+                    setHover(el.id);
+                  }}
+                  onMouseOut={() => {
+                    setHover();
+                  }}
+                  className="stock-list"
+                >
+                  <div
+                    style={{
+                      color: el.todayPrice < el.yesPrice ? "#df514c" : "green",
+                    }}
+                  >
+                    {el.companyName}
+                  </div>
+                  <div>{el.stockExchange}</div>
+                  <div>
+                    {el.yesPrice - el.todayPrice < 0
+                      ? (
+                          ((el.todayPrice - el.yesPrice) / el.yesPrice) *
+                          100
+                        ).toFixed(2)
+                      : (
+                          ((el.yesPrice - el.todayPrice) / el.yesPrice) *
+                          100
+                        ).toFixed(2)}{" "}
+                    %
+                  </div>
+                  {el.todayPrice < el.yesPrice ? (
+                    <KeyboardArrowDownIcon
+                      style={{
+                        color:
+                          el.todayPrice < el.yesPrice ? "#df514c" : "green",
+                      }}
+                    />
+                  ) : (
+                    <KeyboardArrowUpIcon
+                      style={{
+                        color:
+                          el.todayPrice < el.yesPrice ? "#df514c" : "green",
+                      }}
+                    />
+                  )}
+
+                  <div
+                    style={{
+                      color: el.todayPrice < el.yesPrice ? "#df514c" : "green",
+                    }}
+                  >
+                    {el.todayPrice}
+                  </div>
+                  <div
+                    className="stock-pop"
+                    style={{
+                      display: hover === el.id ? "block" : "none",
+                    }}
+                  >
+                    <ErrorOutlineIcon />
+                    <DeleteOutlineIcon
+                      onClick={() => {
+                        showUpdate(el);
+                        console.log(el);
+                      }}
+                    />
+                    <div>N</div>
+                    <div>B</div>
+                  </div>
+                </div>
+              ))}
+        </div>
+      </div>
+
+      {show.length === 0 && stockdata.length === 0 ? (
+        <CircularProgress style={{ marginTop: "200px", overflow: "hidden" }} />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
